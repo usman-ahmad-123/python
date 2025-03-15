999c7b962509f3460a8c26e100b9b62db5195d07
usman-ahmad-123 <mdusmanahmad8@gmail.com>
2025-03-15 20:03:24 +0530

    Initial commit for datascience branch

---
+++ Helloworld.py
@@ -0,0 +1,38 @@
+print("helloworld")
+r=10
+r+=2
+area=3.14*r*r
+print(area)
+print("Enter radius:")
+r=int(input())
+a=3.14*r*r
+print(a)
+str1="usman"
+str2="young"
+print(str1+" "+str2)
+
+# if(r>10):
+    # print("The number is greater than 10")
+# elif(r<10):
+	# print("The number is lessar than 10")
+# else:
+	# print("Equal")
+while(r>10):
+	print("Hellow")
+	r=r-1
+import random
+num = random.random()
+print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
